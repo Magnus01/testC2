4d480cf71af565cf9234e762e941c4832b80a392
unknown <magnus01lysfjord@gmail.com>
2018-10-25 19:57:05 +0200

    '

--- Setup/updatescriptrunner.sh
+++ Setup/updatescriptrunner.sh
@@ -6,7 +6,7 @@
 # Start Docker
 ###########################
 chmod 777 ../API/DockerTimeout.sh
-chmod 777 ../API/Payload/scriptunit.sh
+chmod 777 ../API/Payload/script.sh
 chmod 777 ../API/Payload/javaRunner.sh
 chmod 777 UpdateDocker.sh
 
